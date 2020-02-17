607d5887159118837d490652d0f91ebb66a8dfc8
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-17 00:14:03 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,86 @@
-# Code your solution here!
+require "pry"
+
+
+# def exit_game
+#   input = get_user_input
+#   if input == "exit"
+#     puts "Goodbye!"
+#   end
+#
+# end
+
+# def num_generator
+#   rand(1..6)
+# end
+#
+def prompt_user
+  puts "Guess a number between 1 and 6"
+end
+# # #
+# def get_user_input
+#   gets.chomp
+#
+# end
+#
+#
+# def num_compare
+#   rand_num = num_generator
+#   input = get_user_input
+#
+#   if input == rand_num
+#     puts "You guessed the correct number!"
+#
+#   elsif input == "exit"
+#     puts "Goodbye!"
+#
+#   else
+#     puts "Sorry! The computer guessed #{rand_num}."
+#   end
+# end
+#
+# def run_guessing_game
+#   prompt_user
+#   # exit_game
+#   num_compare
+#
+# end
+
+# def run_guessing_game
+#   rand_num = rand(1..6)
+#
+#   puts "Guess a number between 1 and 6"
+#   input = gets.chomp
+#
+#   # valid_input = "exit" ||
+#   # until condition
+#   #
+#   # end
+#   # if input == "exit"
+#   #   puts "Goodbye!"
+#
+#
+#
+#   if input != rand_num
+#     puts "Sorry! The computer guessed #{rand_num}."
+#   elsif input = rand_num
+#     puts "You guessed the correct number!"
+#   end
+# end
+
+
+def run_guessing_game
+
+  prompt_user
+  rand_num = rand(1..6)
+  input = gets.chomp
+
+   if input == rand_num
+    puts "You guessed the correct number!"
+
+  elsif input == "exit"
+    puts "Goodbye!"
+
+  else
+    puts "Sorry! The computer guessed #{rand_num}."
+  end
+end
