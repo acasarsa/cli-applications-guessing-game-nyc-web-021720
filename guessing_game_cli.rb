0bd650441a368b50ec5c541d9e9f6c600b691fe1
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-17 00:30:20 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -10,32 +10,32 @@ require "pry"
 def prompt_user
   puts "Guess a number between 1 and 6"
 end
-
-def num_generator
-  rand(1..6)
-end
-
-def get_user_input
-  gets.chomp
-end
-
-def num_compare(num)
-  # rand_num = num_generator
-  input = get_user_input
-
-  if input == num
-    puts "You guessed the correct number!"
-  elsif input == "exit"
-    puts "Goodbye!"
-  else
-    puts "Sorry! The computer guessed #{num}."
-  end
-end
-
-def run_guessing_game
-  prompt_user
-  num_compare
-end
+#
+# def num_generator
+#   rand(1..6)
+# end
+#
+# def get_user_input
+#   gets.chomp
+# end
+#
+# def num_compare(num)
+#   # rand_num = num_generator
+#   input = get_user_input
+#
+#   if input == num
+#     puts "You guessed the correct number!"
+#   elsif input == "exit"
+#     puts "Goodbye!"
+#   else
+#     puts "Sorry! The computer guessed #{num}."
+#   end
+# end
+#
+# def run_guessing_game
+#   prompt_user
+#   num_compare
+# end
 
 # def run_guessing_game
 #   rand_num = rand(1..6)
